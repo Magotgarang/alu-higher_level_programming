b49f740ff0c04b546aa725d2a347a1a52da5bfc3
magotgarang <m.guut@alustudent.com>
2023-02-06 06:54:53 -0800

    6-concat.py

--- 6-concat.py
+++ 6-concat.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-str1 = "Helberton"
-str2 = "School"
-strt1  += " " + str2
-print("Welcome to {}!".1format(str1))
+ #!/usr/bin/python3
+ str1 = "Holberton"
+ str2 = "School"
+ str1 += " " + str2
+ print("Welcome to {}!".format(str1))
