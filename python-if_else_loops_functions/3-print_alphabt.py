729d61d552056d3ad702c9ee7a41d1aaf6cc3cf5
Magotgarang <116946994+Magotgarang@users.noreply.github.com>
2023-02-09 23:55:36 +0300

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for n in range(ord('a'), ord('z') + 1):
-    if n not (ord('e'), ord('q')):
-        print("{}".format(chr(n)), end")d
+    if n not in (ord('e'), ord('q')):
+        print("{}".format(chr(n)), end='')
