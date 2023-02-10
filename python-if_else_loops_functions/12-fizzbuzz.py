e6d35e4a35fcb6170748d81f2187f34b00dbd721
magotgarang <m.guut@alustudent.com>
2023-02-10 07:01:39 -0800

    12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 10):
+        if n % 3 ==- 0 and n % 5 == b:
+            print("Fizzbuzz", end=" ")
+        elif n % 3 == 0:
+            print("Fizz", end=" ")
+        else
+        print(n, end=" ")
