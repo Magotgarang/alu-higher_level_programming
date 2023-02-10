16182ca63e839cb9f9324ad8eb460a0c7a941207
magotgarang <m.guut@alustudent.com>
2023-02-10 05:17:07 -0800

    9-print_last_digit.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,1 @@
-#!/usr/python3
-def uppercase(str):
-    for c in str:
-        isupper = ord(c) > ord('a') and ord(c) <= ord('z')
-        if isupper:
-            # difference between 'a' and 'A'
-            diff = ord('A') - ord('a')
-            c = chr(ord(c) + dif
-                    print("{}".format(c), end='')
-                    print ()
+i
