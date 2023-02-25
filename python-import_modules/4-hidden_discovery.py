b8fe703d81f5e9db57c865631b66c1475ad8f813
magotgarang <m.guut@alustudent.com>
2023-02-25 06:24:34 -0800

    updated

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__=='__name__':
+    names = dir(hidden_4)
+    names.sort()
+    for attr in nmaes:
+        if attr[:2] != "__":
+            print(attr)
