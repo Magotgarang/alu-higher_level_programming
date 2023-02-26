3f3091e9b148a3eac4b2eb59d9f28441f83d6d2a
magotgarang <m.guut@alustudent.com>
2023-02-26 03:58:25 -0800

    updated

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-if not my_list:
-    return my_list
+    if not my_list:
+       return my_list
 n_list = list()
 for item in my_list:
     if item % 2 == 0:
