d5a29f08c4e5467c618bd95934f7e92bfbf68f14
magotgarang <m.guut@alustudent.com>
2023-02-26 03:55:56 -0800

    updated

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[])
+def divisible_by_2(my_list=[]):
 if not my_list:
     return my_list
 n_list = list()
@@ -8,4 +8,4 @@ for item in my_list:
         n_list.append(True)
     else:
         n_list.append(False
-                return n_list
+    return n_list
