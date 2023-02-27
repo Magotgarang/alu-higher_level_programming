6facb7d2713b672d0a407970b7b94d1b35ca4e32
magotgarang <m.guut@alustudent.com>
2023-02-27 10:13:47 -0800

    updated

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-def search_replace(my_list, serach, replace):
-    return [element if element != search else replace for element in my_list]
+  #!/usr/bin/python3
+     def search_replace(my_list, serach, replace):
+       return [element if element != search else replace for element in my_list]
