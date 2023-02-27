560ebac400e0dbbef23868a3197881da700826b5
magotgarang <m.guut@alustudent.com>
2023-02-27 09:58:50 -0800

    updated

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 def square_matrix_simple(matrix=[]):
     if not matrix:
         print()
-    return [[element * element for element in row] for row in matrix]]
+    return [[element * element for element in row] for row in matrix]
