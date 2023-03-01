842d6af88f78be5934cafa4f63e775519e4466ca
magotgarang <m.guut@alustudent.com>
2023-03-01 03:30:02 -0800

    updated

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    double_ditc = dict(a_dictionary)
+    for k, v in double_dict.items():
+        double_ditc[k] = v * 2
+        return double_ditc
