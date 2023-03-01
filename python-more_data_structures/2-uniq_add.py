c8a0f8387972e81a272dcf5baa96fe8847b8c0a3
magotgarang <m.guut@alustudent.com>
2023-03-01 02:46:45 -0800

    updated

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set(set_1) & set(set_2)
+def uniq_add(my_list=[]):
+    uniq_list = list()
+    for item in my_list:
+        if item not in uniq_list:
+            uniq_list.append(item)
+    return sum(uniq_list)
