891017b8259afc6dbad5fbf82b74cbe1b6e325fa
magotgarang <m.guut@alustudent.com>
2023-03-24 07:12:05 -0700

    updated

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Are you documented ?"""
+"""Are you docuemnted ?"""
 
 
 import json
@@ -7,5 +7,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """How far now"""
-    with open(filename, "w+) as f:
-    return json.dump(my_obj, f)
+    with open(filename, "w+") as f:
+        return json.dump(my_obj, f)
