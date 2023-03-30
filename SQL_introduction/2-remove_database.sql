a2e6034a9e219d1f073821565192f7002a6d96f2
magotgarang <m.guut@alustudent.com>
2023-03-30 02:32:52 -0700

    updated

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- script that delete hbtn_0c_0 in your mySQL server.
-DELETE DATABASE IF ALREADY EXISTS hbtn_0c_0;
+-- Deletes the database hbtn_0c_0 in MySQL server.
+DROP database IF EXISTS hbtn_0c_0;
