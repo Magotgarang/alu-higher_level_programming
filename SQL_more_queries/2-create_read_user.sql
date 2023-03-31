5510931d12e4b5f7e2b019d36348850b9779536c
magotgarang <m.guut@alustudent.com>
2023-03-31 05:50:20 -0700

    updated

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Script that creates an user and database in mySQL server
+-- Query to create the user 'user_x0d_1' and the database 'hbtn_0d_2' in mySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS- 'user_0d'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'users_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
