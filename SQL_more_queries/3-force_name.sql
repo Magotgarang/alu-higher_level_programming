6b9c9564ca6d1733a8ed416e9dbfde2292aa8312
magotgarang <m.guut@alustudent.com>
2023-03-31 06:01:26 -0700

    updated

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- script that creates the table force_name on mySQL sever.
-CREATE TABLE IF NULL EXISTS force_name (
-	id INT, 
-	name VARCHAR(256) NOT NULL
-	);
+-- script that creates the table force_name on MySQL server.
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
