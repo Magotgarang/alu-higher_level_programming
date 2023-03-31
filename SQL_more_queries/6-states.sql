daf535f5a952bce17bd60ae71930041645556903
magotgarang <m.guut@alustudent.com>
2023-03-31 06:35:16 -0700

    updated

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- table states (in the database hbtn_0d_usa) on your mySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE DATABASE IF NOT EXISTS states (
+CREATE DATABASE IF NOT EXISTS states(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(256)
 	);
