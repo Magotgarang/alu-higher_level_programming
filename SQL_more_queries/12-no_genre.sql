26ceb13301529ed647821d52415fe916b7c475ab
magotgarang <m.guut@alustudent.com>
2023-03-31 07:45:53 -0700

    updated

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
-SELECT tv_shows.title, tv_show_genres.genre_id -- Queryto join cities and states
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id is NULL
 ORDER BY tv_shows.title;
