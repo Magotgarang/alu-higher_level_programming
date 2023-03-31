2f300a1af18adfa7281353944c8d736596a8cb6e
magotgarang <m.guut@alustudent.com>
2023-03-31 06:11:23 -0700

    updated

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
--- creates the table id_not_null on your mySQL server.
-CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL DEFUALT 1, name VARCHAR(256));
+-- creates the table id_not_null on your MySQL server.
+CREATE table IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
