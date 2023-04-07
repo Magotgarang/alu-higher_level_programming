c8401d9972963f84a75078138f1aa03bbbc7b94a
Magotgarag <m.guut@alustudent.com>
2023-04-07 12:28:49 +0000

    updated

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#/bin/bash
-# Script that shows the content-length from a HTTP request
-curl -sI "$" | grep "content-length:" |cut -d " " -f 2
+#!/bin/bash
+# Script that shows the Content-Length from a HTTP request
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
