e67d2c23eb352c7c04b6244d93b6160d69e185b5
Magotgarang <m.guut@alustudent.com>
2023-04-07 16:34:50 +0000

    updated

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -4,6 +4,7 @@ import requests
 import sys
 
 if __name__ == '__main__':
- url = sys.argv[1]
- response = requests.get(url)
- print("{}".format(response.headers.get("X-Request-Id")))
+    url = sys.argv[1]
+    response = requests.get(url)
+    print("{}".format(response.headers.get("X-Request-Id")))
+Footer
