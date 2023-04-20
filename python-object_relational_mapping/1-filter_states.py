a0e92eadb1c286ca7281e521c663d0874a76c718
magotgarang <m.guut@alustudent.com>
2023-04-20 04:39:07 -0700

    updated

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,6 +5,7 @@ Lists all states with a name starting with N
 import sys
 import MySQLdb
 
+
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
             db=sys.argv[3], port=3306)
