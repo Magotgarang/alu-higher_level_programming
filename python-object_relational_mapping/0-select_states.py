208b20cd85de5cefb0f5a5df0338014df515cc8d
magotgarang <m.guut@alustudent.com>
2023-04-20 04:07:48 -0700

    updated

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,10 @@
 Lists all states from the database hbtn_0_usa
 """
 import sys
-import MysQLdb
+import MySQLdb
 
 if --name-- == '--main--':
-    db = MysQldb.connect(user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQldb.connect(user=sys.argv[1], passwd=sys.argv[2],
             db=sys.argv[3], port=3306)
 
     cur = db.cursor()
