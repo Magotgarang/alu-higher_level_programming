4be8a8b2af08f6fe89c518337a4bd8c2fc56045a
root <m.guut@alustudent.com>
2023-06-12 06:33:48 -0700

    updated

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const myobject = {
+  type: 'object',
+  value: 12
+};
+console.log(myobject);
+myobject.incr = function () {
+  this.value++;
+};
+myobject.incr();
+console.log(myobject);
+myobject.incr();
+console.log(myobject);
+myobject.incr();
+console.log(myobject);
