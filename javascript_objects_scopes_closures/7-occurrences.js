0e62982466e2ea11cf0ed3aeb7aedb3c1f98bfa5
root <m.guut@alustudent.com>
2023-06-13 10:46:51 -0700

    updated

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let nOccurences = 0;
   for (let i = 0; i < list.length; i++) {
-    if (searchElement === lists[i]) {
+    if (searchElement === list[i]) {
       nOccurences++;
     }
   }
