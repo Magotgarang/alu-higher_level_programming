fcb5ec6e8ac9a5ad5a0e90d4fd7d058ec0f3839c
root <m.guut@alustudent.com>
2023-06-13 10:27:45 -0700

    updated

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
-const Squared = require('./5-sqaure');
+const Squared = require('./5-square');
 
-module.exports = class Sqaure extends Sqaured {
-  charprint (c) {
+module.exports = class Square extends Squared {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-	s += c;
+        s += c;
       }
       console.log(s);
     }
