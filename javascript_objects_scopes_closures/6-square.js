b4817ad6b2c4a218586e6bd76bda5d434063b120
root <m.guut@alustudent.com>
2023-06-13 10:13:47 -0700

    updated

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Square = require('./5-sqaure');
+
+module.exports = class Sqaure extends Sqaured {
+  charprint (c) {
+    if (c === undefined) {
+     c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+	s += c;
+      }
+      console.log(s);
+    }
+  }
+};
