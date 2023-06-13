8e517c07cf7d39484aa7bd8c4f0ce9810b673578
root <m.guut@alustudent.com>
2023-06-13 03:47:09 -0700

    updated

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,27 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-constructor (w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-print () {
+  print () {
     for (let i = 0; i < this.height; i++) {
-     console.log('X'.repeat(this.width));
+      co.nsole.log('X'.repeat(this.width));
     }
   }
 
-rotate () {
-  const temp = this.height;
-  this.height = this.width;
-  this.width = temp;
-}
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
 
-double () {
-  this.height = this.height * 2;
-  this.width = this.width * 2;
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
 }
 };
