15f75ab60df72443886e41b7bdd6593301843089
root <m.guut@alustudent.com>
2023-06-14 03:08:23 -0700

    updated

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -7,5 +7,5 @@ const filepath = process.argv[2];
 request.readfile(filepath, function (err, data) {
   if (err) console.log(err);
   const content = data;
-  console.log(content.toString();
+  console.log(content.toString());
 });
