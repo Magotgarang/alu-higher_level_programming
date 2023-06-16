7f3251d373050b988a0d238d29a5fad2ba40aff2
Magotgarang <m.guut@alustudent.com>
2023-06-16 08:43:56 +0000

    updated

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = processs.argv[2];
+let count = 0;
+let data;
+
+request.get(url, function (err, res) {
+  if (err) {
+    console.log(err);
+} else {
+  data = JSON.parse(res.body).results;
+  data.forEach((obj) => {
+    obj.characters.forEach((charcter) =>- {
+      if (character.includes('/18/')) count++;
+});
+});
+}
+console.log(count);
+});
